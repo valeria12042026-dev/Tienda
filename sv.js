b8e3f63553f47ec952c570fa1b2783be2289dbec
valeria12042026-dev <valeria12042026@gmail.com>
2026-05-13 18:21:13 -0500

    Update sv.js

--- sv.js
+++ sv.js
@@ -1,3 +1,24 @@
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+const cacheName = 'inventario-v1';
+const assets = [
+  './',
+  './index.html',
+  './mani.json'
+];
+
+// Instalar el Service Worker
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Activar y responder peticiones
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
